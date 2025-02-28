90e054d057bc6236296b1ecf651a2cedc6f97bbc
futurehappyman <stepannikonov6294@gmail.com>
2025-02-28 10:43:04 +0100

    -- created express server with port 3000 -- get method to communicate with localhost5500(req, res) and work specifically for the cryptonews -- get method to ask cryptopanic server for data, receive it and send back to the fetchNews that asked for the data. -- app.listen to start and make it listen(works with fetch and etc)

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+const API_KEY = 'e7fcfa7dbeba45e05c48d9e21d36328c94b23f37';
+app.get("/api/posts", async (req, res) => { 
+   try { 
+      const apiUrl = `https://cryptopanic.com/api/v1/posts/?auth_token=${API_KEY}&public=true`;
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      res.json(data);
+   } catch (error) {
+      console.error(error);
+   }
+});
+      
+app.listen(PORT, () => console.log("server port: http://localhost:3000"));
+
