10f3adb38b7825063b672b60c671d6d0da1e9643
futurehappyman <stepannikonov6294@gmail.com>
2025-02-28 10:40:38 +0100

    list of responsibilities for now: -- asking localhost3000 for data and receiving it, converting to object -- displaying news on main page -- displaying todays date

---
+++ js/app.js
@@ -0,0 +1,39 @@
+async function fetchNews() {
+  try {
+    const response = await fetch("http://localhost:3000/api/posts");
+    const data = await response.json();
+    displayNews(data.results);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function displayNews(newsData) {
+  const container = document.querySelector(".news-container");
+  container.innerHTML = "";
+
+  newsData.forEach((news) => {
+    const article = document.createElement("article");
+    article.classList.add("news-item");
+
+    article.innerHTML = `
+       <h2><a class="anchor-title" href="${news.url}" target="_blank">${news.title}</a></h2>
+       <a>Source: ${news.source.title}</a> 
+       <p>Published: ${new Date(news.published_at).toLocaleString()}</p>
+     `;
+    container.appendChild(article);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", fetchNews);
+
+function displayData() {
+  const dataElement = document.querySelector(".date-time");
+  let date = new Date()
+  let today = date.toLocaleDateString();
+  if (today) {
+    dataElement.textContent = today;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", displayData);
