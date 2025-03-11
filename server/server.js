d6f76c51dccd7c49d7667aadcae6e05f47e3346a
futurehappyman <stepannikonov6294@gmail.com>
2025-03-11 22:41:58 +0100

    simple validation

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
+
 
 const API_KEY = 'e7fcfa7dbeba45e05c48d9e21d36328c94b23f37';
 app.get("/api/posts", async (req, res) => { 
