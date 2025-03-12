9bf76ac90a1c3469b2cfc4be776738c4c46f671f
futurehappyman <stepannikonov6294@gmail.com>
2025-03-12 10:49:18 +0100

    transmitting files to the server 3000

--- public/js/login.js
+++ public/js/login.js
@@ -2,21 +2,23 @@ function validateInputForm(event) {
   const emailInput = document.getElementById("email-input");
   const passwordInput = document.getElementById("password-input");
 
+  
   const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const passwordFormat = /([a-zA-Z0-9]{6,})/;
 
   if (emailInput.value.trim() === "" || !emailFormat.test(emailInput.value)) {
-    alert("please enter valid Email!");
-    event.preventDefault();
+    alert("Enter correct email!");
+    event.preventDefault(); 
     return false;
   }
   if (passwordInput.value.length < 6 || !passwordFormat.test(passwordInput.value)) {
-    alert(`Enter Valid password(length min 6, only numbers and letters)`);
+    alert("Enter correct password (minimum length 6)");
     event.preventDefault();
     return false;
   }
   return true;
 }
+
 
 const loginForm = document.querySelector("form");
 if (loginForm) {
--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
+app.options('*', cors());
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 const API_KEY = 'e7fcfa7dbeba45e05c48d9e21d36328c94b23f37';
 app.get("/api/posts", async (req, res) => { 
@@ -20,5 +24,26 @@ app.get("/api/posts", async (req, res) => {
    }
 });
 
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'login.html'));
+});
 
-app.listen(PORT, () => console.log("server port: http://localhost:3000"));
+
+app.post("/login", (req, res) => {
+  const email = req.body["email-input"];
+  const password = req.body["password-input"];
+  console.log("Получены данные логина:", email, password);
+
+  
+  if (email === "user@example.com" && password === "123456") {
+    res.redirect("/dashboard");
+  } else {
+    res.send('Неверный email или пароль. <a href="/login">Попробуйте снова</a>');
+  }
+});
+
+app.get("/dashboard", (req, res) => {
+  res.send("Добро пожаловать в Dashboard!");
+});
+
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
